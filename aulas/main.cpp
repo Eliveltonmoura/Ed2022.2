72247805ebc39806bf870c7595fe1353eaa672cb
Elivelton Moura <78855380+Eliveltonmoura@users.noreply.github.com>
2022-08-22 19:44:53 -0300

    add

--- aulas/main.cpp
+++ aulas/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Olá pessoas\n";
+    int vetor[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << vetor[i] << "";
+    }
 }
