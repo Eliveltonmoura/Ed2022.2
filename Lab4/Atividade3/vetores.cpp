aceb94ae63af08812c481f2310ad3e1c7ffc30f5
Elivelton Moura <78855380+Eliveltonmoura@users.noreply.github.com>
2022-09-30 15:45:47 -0300

    add

--- Lab4/Atividade3/vetores.cpp
+++ Lab4/Atividade3/vetores.cpp
@@ -8,12 +8,26 @@ void vetorNormal(vector<int> v, int n)
 
     if (n == 0)
     {
-        cout << v[n];
+        cout << "vet : [ " << v[n];
     }
     else
     {
         vetorNormal(v, n - 1);
-        cout << v[n];
+        cout << " " << v[n];
+    }
+}
+
+void vetorInvt(vector<int> v, int n) // investe um vetor
+{
+
+    if (n == 0)
+    {
+        cout << " " << v[n];
+    }
+    else
+    {
+        cout << " " << v[n];
+        vetorInvt(v, n - 1);
     }
 }
 
@@ -41,6 +55,25 @@ int vetorMut(vector<int> v, int n) // recursão para multiplicar os elentos do v
     }
 }
 
+int vetorMin(vector<int> v, int n, int i)
+{
+    if (n == 0)
+    {
+        return v[i];
+    }
+    else
+    {
+        if (v[n] < v[i])
+        {
+            return vetorMin(v, n - 1, n);
+        }
+        else
+        {
+            return vetorMin(v, n - 1, i);
+        }
+    }
+}
+
 int main()
 {
     string line;
@@ -52,13 +85,24 @@ int main()
     {
         vet.push_back(n);
     }
+
     int tam = vet.size();
 
-    vetorNormal(vet, tam);          // retornar os elementos de um vertor
-    int soma = vetorSoma(vet, tam); // recursão de soma
-    int mult = vetorMut(vet, tam);  // recusão de multiplica
+    vetorNormal(vet, tam - 1); // retornar os elementos de um vertor
+    cout << " ]" << endl;
+    cout << "rvet: [";
+    vetorInvt(vet, tam - 1); // investe um vetor
+    cout << " ]";
+    int soma = vetorSoma(vet, tam);      // recursão de soma
+    int mult = vetorMut(vet, tam);       // recusão de multiplica
+    int min = vetorMin(vet, tam - 1, 0); // menor elemento
 
     cout << endl;
-    cout << "sum: " << soma << endl;
+    cout << "sum : " << soma << endl;
     cout << "mult: " << mult << endl;
+    cout << "min : " << min << endl;
+
+    cout << "inv : [";
+    vetorInvt(vet, tam - 1);
+    cout << " ]" << endl;
 }
